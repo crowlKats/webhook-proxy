6b30e36688b16a1fe9e4ec1d75d56b967662db3c
crowlkats <13135287+crowlKats@users.noreply.github.com>
2021-03-29 14:39:14 +0200

    log

--- receive.ts
+++ receive.ts
@@ -20,6 +20,7 @@ export async function receive(
 
   const body = new Uint8Array(await req.arrayBuffer());
   const calc = hmac("sha256", Deno.env.get("GITHUB_WEBHOOK_SECRET")!, body);
+  console.log(sig, calc);
   if ("sha256=" + calc.toString() !== sig) {
     return new Response(undefined, {
       status: Status.InternalServerError,
